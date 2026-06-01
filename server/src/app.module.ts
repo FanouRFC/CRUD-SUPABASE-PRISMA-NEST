fb09c2f0f65381bb6b93291f7335892f70bb494f
unknown <fanouomega@gmail.com>
2026-06-01 22:26:15 +0300

    feat(user): injection prismaService

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { PrismaModule } from './prisma/prisma.module';
+import { PostModule } from './post/post.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true}), PrismaModule],
+  imports: [ConfigModule.forRoot({isGlobal: true}), PostModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
