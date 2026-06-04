19e966ce4bebc61e567977899a57db0072e233a0
unknown <fanouomega@gmail.com>
2026-06-04 20:07:11 +0300

    operation delete et update

--- client/src/components/table.tsx
+++ client/src/components/table.tsx
@@ -11,9 +11,14 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Button } from "./ui/button";
-import { Input } from "@base-ui/react";
+import { Input, type BaseUIEvent } from "@base-ui/react";
 import { postApi } from "@/api/post.api";
 import { userApi } from "@/api/user.api";
+import { useEffect, useState, type ChangeEvent } from "react";
+import { Spinner } from "./ui/spinner";
+
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
 type MyTableProps = {
   header: string[];
@@ -46,7 +51,7 @@ export default function MyTable({
                 <td className="border px-2">{u.email}</td>
                 <td className="border px-2 flex justify-center">
                   <div className="flex flex-row gap-1">
-                    <DialogEdit type={contentType} />
+                    <DialogEdit type={contentType} data={u} />
                     <DialogDelete type={contentType} id={parseInt(u.id)} />
                   </div>
                 </td>
@@ -64,7 +69,7 @@ export default function MyTable({
               <td className="border px-2">{p.author.name ?? ""}</td>
               <td className="border px-2 flex justify-center">
                 <div className="flex flex-row gap-1">
-                  <DialogEdit type={contentType} />
+                  <DialogEdit type={contentType} data={p} />
                   <DialogDelete type={contentType} id={parseInt(p.id)} />
                 </div>
               </td>
@@ -82,14 +87,22 @@ type DialogDeleteProps = {
 };
 
 export function DialogDelete({ type, id }: DialogDeleteProps) {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   async function deleteData() {
     try {
+      setIsLoading(true);
       if (type == "post") {
         const req = await postApi.deletePost(id);
+        setIsLoading(false);
+        window.location.reload();
       } else {
         const req = await userApi.deleteUser(id);
+        setIsLoading(false);
+        window.location.reload();
       }
     } catch (error) {
+      setIsLoading(false);
       console.log(error);
     }
   }
@@ -122,30 +135,101 @@ export function DialogDelete({ type, id }: DialogDeleteProps) {
               </Button>
             }
           />
-          <DialogClose
-            render={
-              <Button
-                type="button"
-                className="cursor-pointer bg-red-400 text-black"
-                onClick={async () => {
-                  deleteData();
-                }}
-              >
-                Confirmer
-              </Button>
-            }
-          />
+          {isLoading ? (
+            <Button
+              type="button"
+              className="cursor-pointer bg-red-400 text-black"
+            >
+              <Spinner />
+            </Button>
+          ) : (
+            <Button
+              type="button"
+              className="cursor-pointer bg-red-400 text-black"
+              onClick={async () => {
+                deleteData();
+              }}
+            >
+              Confirmer
+            </Button>
+          )}
         </DialogFooter>
       </DialogContent>
     </Dialog>
   );
 }
 
-type DialogEditProps = {
-  type: "user" | "post";
-};
+type DialogEditProps =
+  | {
+      type: "user";
+      data: user;
+    }
+  | {
+      type: "post";
+      data: post;
+    };
 
-export function DialogEdit({ type }: DialogEditProps) {
+export function DialogEdit({ type, data }: DialogEditProps) {
+  const [userData, setUserData] = useState<user | undefined>(
+    type == "user" ? data : undefined,
+  );
+  const [postData, setPostData] = useState<post | undefined>(
+    type == "post" ? data : undefined,
+  );
+
+  const [radioData, setRadioData] = useState<string>(
+    postData?.published ? "published" : "not_published",
+  );
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  async function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    if (type == "user") {
+      setUserData({ ...userData, [e.target.name]: e.target.value });
+    } else {
+      setPostData({ ...postData, [e.target.name]: e.target.value });
+    }
+  }
+
+  async function handleRadioChange(
+    e: BaseUIEvent<ChangeEvent<HTMLInputElement, Element>>,
+  ) {
+    setRadioData(e.target.value);
+  }
+
+  async function updateData() {
+    if (type === "user") {
+      try {
+        setIsLoading(true);
+        var dataUserApi = {
+          name: userData?.name,
+          email: userData?.email,
+        };
+        const api = await userApi.updateUser(parseInt(data.id), dataUserApi);
+        setIsLoading(false);
+        window.location.reload();
+      } catch (error) {
+        setIsLoading(false);
+        console.log(error);
+      }
+    } else {
+      try {
+        setIsLoading(true);
+        var dataPostApi = {
+          title: postData?.title,
+          content: postData?.content,
+          published: radioData == "published" ? true : false,
+        };
+        const api = await postApi.updatePost(parseInt(data.id), dataPostApi);
+        setIsLoading(false);
+        window.location.reload();
+      } catch (error) {
+        setIsLoading(false);
+        console.log(error);
+      }
+    }
+  }
+
   return (
     <Dialog>
       <DialogTrigger
@@ -167,33 +251,101 @@ export function DialogEdit({ type }: DialogEditProps) {
               <div className="flex flex-col gap-3">
                 <div className="flex flex-col gap-2">
                   <p>Name</p>
-                  <Input type="text" className={"border border-b rounded-md"} />
+                  <Input
+                    type="text"
+                    name="name"
+                    className={"border border-b rounded-md"}
+                    value={userData?.name ?? ""}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                      handleChange(e);
+                    }}
+                    required
+                  />
                 </div>
                 <div className="flex flex-col gap-2">
                   <p>Email</p>
-                  <Input type="text" className={"border border-b rounded-md"} />
+                  <Input
+                    type="text"
+                    name="email"
+                    className={"border border-b rounded-md"}
+                    value={userData?.email ?? ""}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                      handleChange(e);
+                    }}
+                    required
+                  />
                 </div>
               </div>
             ) : (
               <div className="flex flex-col gap-3">
                 <div className="flex flex-col gap-2">
                   <p>title</p>
-                  <Input type="text" className={"border border-b rounded-md"} />
+                  <Input
+                    type="text"
+                    name="title"
+                    className={"border border-b rounded-md"}
+                    value={postData.title ?? ""}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                      handleChange(e);
+                    }}
+                    required
+                  />
                 </div>
                 <div className="flex flex-col gap-2">
                   <p>content</p>
-                  <Input type="text" className={"border border-b rounded-md"} />
+                  <Input
+                    type="text"
+                    name="content"
+                    className={"border border-b rounded-md"}
+                    value={postData?.content ?? ""}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                      handleChange(e);
+                    }}
+                    required
+                  />
                 </div>
                 <div className="flex flex-col gap-2">
                   <p>published</p>
-                  <Input type="text" className={"border border-b rounded-md"} />
+                  <RadioGroup
+                    defaultValue={
+                      postData.published ? "published" : "not_published"
+                    }
+                    value={radioData}
+                    onChange={(
+                      e: BaseUIEvent<ChangeEvent<HTMLInputElement, Element>>,
+                    ) => {
+                      handleRadioChange(e);
+                    }}
+                    required
+                    className="w-fit"
+                  >
+                    <div className="flex items-center gap-3">
+                      <RadioGroupItem value="published" id="r2" />
+                      <Label htmlFor="r2">Publié</Label>
+                    </div>
+                    <div className="flex items-center gap-3">
+                      <RadioGroupItem value="not_published" id="r3" />
+                      <Label htmlFor="r3">Non publié</Label>
+                    </div>
+                  </RadioGroup>
+                  <Input
+                    type="text"
+                    name="published"
+                    className={"border border-b rounded-md"}
+                    value={postData?.published ? "True" : "False"}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                      handleChange(e);
+                    }}
+                  />
                 </div>
                 <div className="flex flex-col gap-2">
                   <p>Author</p>
                   <Input
                     type="text"
                     className={"border border-b rounded-md"}
+                    value={postData.author.name}
                     readOnly
+                    required
                   />
                 </div>
               </div>
@@ -212,13 +364,21 @@ export function DialogEdit({ type }: DialogEditProps) {
               </Button>
             }
           />
-          <DialogClose
-            render={
-              <Button type="button" className="cursor-pointer bg-blue-500">
-                Confirmer
-              </Button>
-            }
-          />
+          {isLoading ? (
+            <Button type="button" className="cursor-pointer bg-blue-500">
+              <Spinner />
+            </Button>
+          ) : (
+            <Button
+              type="button"
+              className="cursor-pointer bg-blue-500"
+              onClick={() => {
+                updateData();
+              }}
+            >
+              Confirmer
+            </Button>
+          )}
         </DialogFooter>
       </DialogContent>
     </Dialog>
