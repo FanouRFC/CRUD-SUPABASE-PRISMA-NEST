7a11ddeca12e53118acd90cfa9332189c3a74dd1
unknown <fanouomega@gmail.com>
2026-06-04 18:14:51 +0300

    ajout dialog dans table

--- client/src/components/table.tsx
+++ client/src/components/table.tsx
@@ -1,5 +1,17 @@
 import type { post, user } from "../types/tableType";
 import { Trash, SquarePen } from "lucide-react";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "./ui/button";
+import { Input } from "@base-ui/react";
 
 type MyTableProps = {
   header: string[];
@@ -32,8 +44,8 @@ export default function MyTable({
                 <td className="border px-2">{u.email}</td>
                 <td className="border px-2 flex justify-center">
                   <div className="flex flex-row gap-1">
-                    <Trash className="cursor-pointer" />
-                    <SquarePen className="cursor-pointer" />
+                    <DialogEdit type={contentType} />
+                    <DialogDelete type={contentType} />
                   </div>
                 </td>
               </tr>
@@ -50,8 +62,8 @@ export default function MyTable({
               <td className="border px-2">{p.author.name ?? ""}</td>
               <td className="border px-2 flex justify-center">
                 <div className="flex flex-row gap-1">
-                  <Trash className="cursor-pointer" />
-                  <SquarePen className="cursor-pointer" />
+                  <DialogEdit type={contentType} />
+                  <DialogDelete type={contentType} />
                 </div>
               </td>
             </tr>
@@ -59,5 +71,134 @@ export default function MyTable({
         })}
       </tbody>
     </table>
+  );
+}
+
+export function DialogDelete({ type }: DialogProps) {
+  return (
+    <Dialog>
+      <DialogTrigger
+        render={
+          <Button className={"bg-transparent cursor-pointer"}>
+            <Trash className=" size-5" color="black" />
+          </Button>
+        }
+      ></DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Êtes-vous absolument sûr(e)</DialogTitle>
+          <DialogDescription>
+            Cette action est irréversible. Elle supprimera définitivement vos
+            données de nos serveurs.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="sm:justify-end">
+          <DialogClose
+            render={
+              <Button
+                type="button"
+                className="cursor-pointer bg-transparent text-black"
+              >
+                Fermer
+              </Button>
+            }
+          />
+          <DialogClose
+            render={
+              <Button
+                type="button"
+                className="cursor-pointer bg-red-400 text-black"
+              >
+                Confirmer
+              </Button>
+            }
+          />
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+type DialogProps = {
+  type: "user" | "post";
+};
+
+export function DialogEdit({ type }: DialogProps) {
+  return (
+    <Dialog>
+      <DialogTrigger
+        render={
+          <Button className={"bg-transparent cursor-pointer"}>
+            <SquarePen className=" size-5" color="black" />
+          </Button>
+        }
+      ></DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>
+            Modification {type == "user" ? "user" : "post"}
+          </DialogTitle>
+        </DialogHeader>
+        <DialogDescription
+          render={
+            type == "user" ? (
+              <div className="flex flex-col gap-3">
+                <div className="flex flex-col gap-2">
+                  <p>Name</p>
+                  <Input type="text" className={"border border-b rounded-md"} />
+                </div>
+                <div className="flex flex-col gap-2">
+                  <p>Email</p>
+                  <Input type="text" className={"border border-b rounded-md"} />
+                </div>
+              </div>
+            ) : (
+              <div className="flex flex-col gap-3">
+                <div className="flex flex-col gap-2">
+                  <p>title</p>
+                  <Input type="text" className={"border border-b rounded-md"} />
+                </div>
+                <div className="flex flex-col gap-2">
+                  <p>content</p>
+                  <Input type="text" className={"border border-b rounded-md"} />
+                </div>
+                <div className="flex flex-col gap-2">
+                  <p>published</p>
+                  <Input type="text" className={"border border-b rounded-md"} />
+                </div>
+                <div className="flex flex-col gap-2">
+                  <p>Author</p>
+                  <Input
+                    type="text"
+                    className={"border border-b rounded-md"}
+                    readOnly
+                  />
+                </div>
+              </div>
+            )
+          }
+        ></DialogDescription>
+
+        <DialogFooter className="sm:justify-end">
+          <DialogClose
+            render={
+              <Button
+                type="button"
+                className="cursor-pointer bg-transparent text-black"
+              >
+                Fermer
+              </Button>
+            }
+          />
+          <DialogClose
+            render={
+              <Button type="button" className="cursor-pointer bg-blue-500">
+                Confirmer
+              </Button>
+            }
+          />
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
